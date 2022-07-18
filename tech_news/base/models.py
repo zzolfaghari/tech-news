510891e10f9f949b5dbf91c6f5d9fb2b36f10769
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-07-18 14:27:48 +0430

    description field added

--- tech_news/base/models.py
+++ tech_news/base/models.py
@@ -8,6 +8,7 @@ from base.enums import StatusType
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
     thumbnail = models.ImageField(upload_to="images")
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
