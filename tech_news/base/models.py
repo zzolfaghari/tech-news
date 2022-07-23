a29d123b0cfb41f7da11e062fc38dad0e4b61e65
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-07-23 21:16:33 +0430

    category model added

--- tech_news/base/models.py
+++ tech_news/base/models.py
@@ -7,10 +7,27 @@ from base.enums import StatusType
 from shared.utils import TechNewsUtils
 
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    status = models.BooleanField(default=True, verbose_name="آیا نمایش داده شود؟")
+    position = models.IntegerField()
+
+    class Meta:
+        verbose_name = "دسته‌بندی"
+        verbose_name_plural = "دسته‌بندی‌ها"
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField()
+    category = models.ManyToManyField(Category, verbose_name="دسته‌بندی")
     thumbnail = models.ImageField(upload_to="images")
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
