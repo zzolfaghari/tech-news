604686af6b49a8eeffc9a27a8c85e9e74bbfef65
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-07-23 22:02:39 +0430

    refactor admin panel

--- tech_news/base/admin.py
+++ tech_news/base/admin.py
@@ -4,20 +4,28 @@ from base.models import Article, Category
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('position','title', 'slug', 'status')
+    list_display = ('position', 'title', 'slug', 'status')
     list_filter = ('status',)
     search_fields = ('title', 'slug')
     prepopulated_fields = dict(slug=('title',))
 
 
+
+
 admin.site.register(Category, CategoryAdmin)
 
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'jpublish', 'status')
+    list_display = ('title', 'slug', 'jpublish', 'status', 'show_category')
     list_filter = ('publish', 'status')
     search_fields = ('title', 'description')
     prepopulated_fields = dict(slug=('title',))
     ordering = ['status', 'publish']
+
+    def show_category(self, obj):
+        return "، ".join([category.title for category in obj.category.all()])
+
+    show_category.short_description = "دسته‌بندی"
 
 
 admin.site.register(Article, ArticleAdmin)
