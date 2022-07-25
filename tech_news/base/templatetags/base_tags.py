9e83a51bebadf232f371b4858ff215982574d693
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-07-25 21:45:53 +0430

    categories refactored

--- tech_news/base/templatetags/base_tags.py
+++ tech_news/base/templatetags/base_tags.py
@@ -1,8 +1,17 @@
 from django import template
 
+from base.models import Category
+
 register = template.Library()
 
 
 @register.simple_tag()
 def title(data="Tech News Blog"):
     return data
+
+
+@register.inclusion_tag("base/partials/category_navbar.html")
+def category_navbar():
+    return {
+        "category": Category.objects.filter(status=True)
+    }
