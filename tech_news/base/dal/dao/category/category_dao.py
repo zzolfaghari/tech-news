df0430135a17d8b83bef1c87c55b3e0e8bbd8a3a
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-07-27 13:44:35 +0430

    category dao refactored

--- tech_news/base/dal/dao/category/category_dao.py
+++ tech_news/base/dal/dao/category/category_dao.py
@@ -3,8 +3,8 @@ from base.models import Category
 
 class CategoryDao:
     def __init__(self):
-        super(CategoryDao, self).__init__()
+        super().__init__()
 
     def get_category_by_status_and_slug(self, status, slug):
-        categories = Category.objects.filter(status=status, slug=slug)
+        categories = Category.objects.filter(status=status, slug=slug).first()
         return categories
