bbf45bb19edd26526dc444da134aeeda4221926f
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-07-29 19:55:46 +0430

    refactor category to listview

--- tech_news/base/controller/categories/categories_controller.py
+++ tech_news/base/controller/categories/categories_controller.py
@@ -1,24 +1,29 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
 
 from base.logic.category.category_logic import CategoryLogic
 
 
-class CategoryController(View):
+class CategoryController(ListView):
+    paginate_by = 5
+    template_name = 'base/category_list.html'
+
     def __init__(self):
         super().__init__()
         self.category_logic = CategoryLogic()
 
-    def get(self, request, slug, page=1):
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
         category = self.category_logic.get_category_by_status_and_slug(status=True, slug=slug)
-        articles_list = category.articles.published()
-        paginator = Paginator(articles_list, 1)
-        paginated_articles = paginator.get_page(page)
-        context = {
-            'category': category,
-            'articles': paginated_articles
-        }
-        return render(request, template_name='base/category.html', context=context)
+        return category.articles.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
+
 
 
