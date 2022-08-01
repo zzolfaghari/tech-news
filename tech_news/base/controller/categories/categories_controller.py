d0a264ef0c9760422b8c64ad4cbdd193ce135088
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-08-01 23:27:36 +0430

    category view refactored

--- tech_news/base/controller/categories/categories_controller.py
+++ tech_news/base/controller/categories/categories_controller.py
@@ -1,6 +1,3 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
-from django.views import View
 from django.views.generic import ListView
 
 from base.logic.category.category_logic import CategoryLogic
