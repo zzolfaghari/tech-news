3b44fd7a32a7194c77735cc46df206769a245878
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-08-05 11:13:48 +0430

    account urls refactored

--- tech_news/tech_news/urls.py
+++ tech_news/tech_news/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', include('base.urls'))
+                  path('', include('base.urls')),
+                  path('account/', include('account.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +\
               (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
