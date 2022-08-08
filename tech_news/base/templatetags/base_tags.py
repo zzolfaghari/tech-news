558929e34a8e084f39a53739a86dbc7a7cd6f627
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-08-08 19:35:54 +0430

    active menu view added

--- tech_news/base/templatetags/base_tags.py
+++ tech_news/base/templatetags/base_tags.py
@@ -15,3 +15,13 @@ def category_navbar():
     return {
         "category": Category.objects.filter(status=True)
     }
+
+
+@register.inclusion_tag("registration/partials/link.html")
+def link(request, link_name, content):
+    return {
+        "request": request,
+        "link_name": link_name,
+        "link": f"account:{link_name}",
+        "content": content
+    }
