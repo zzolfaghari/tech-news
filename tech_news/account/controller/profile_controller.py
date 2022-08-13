9cf58013fe92b0f6fbbed1fc907184583b11f0d1
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-08-13 20:49:44 +0430

    form of profile refactored

--- tech_news/account/controller/profile_controller.py
+++ tech_news/account/controller/profile_controller.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 
+from account.forms import ProfileForm
 from account.logic.user_logic import UserLogic
 from account.models import User
 
@@ -8,7 +9,7 @@ from account.models import User
 class ProfileView(UpdateView):
     model = User
     template_name = "registration/profile.html"
-    fields = ["username", "email", "first_name", "last_name", "special_user", "is_author"]
+    form_class = ProfileForm
     success_url = reverse_lazy("account:profile")
 
     def get_object(self, queryset=None):
