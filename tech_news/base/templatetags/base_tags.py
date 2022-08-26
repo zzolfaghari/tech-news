3ac3ce3afc47c89696c97789a16c3660669377f5
zzolfaghari <zahrazolfaghari00@gmail.com>
2022-08-26 18:13:33 +0430

    most comments article templates tag added

--- tech_news/base/templatetags/base_tags.py
+++ tech_news/base/templatetags/base_tags.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
 from django import template
+from django.contrib.contenttypes.models import ContentType
 from django.db.models import Count, Q
 
 from base.models import Category, Article
@@ -28,6 +29,19 @@ def popular_articles():
             count=Count('hits', filter=Q(articlehit__created__gt=last_month))
         ).order_by('-count', '-publish')[:5],
         "title": "مقالات پربازدید ماه"
+    }
+
+
+@register.inclusion_tag("base/partials/sidebar.html")
+def hot_articles():
+    last_month = datetime.today() - timedelta(days=30)
+    content_type_id = ContentType.objects.get(app_label='base', model='article').id
+    return {
+        "articles": Article.objects.published().annotate(
+            count=Count('comments',
+                        filter=Q(comments__posted__gt=last_month) & Q(comments__content_type_id=content_type_id))
+        ).order_by('-count', '-publish')[:5],
+        "title": "مقالات داغ ماه"
     }
 
 
